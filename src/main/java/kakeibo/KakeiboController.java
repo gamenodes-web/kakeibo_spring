228ff78d521d964100baf6b32708ee31f2a6a1db
gamenodes <curry.and.takoyaki.01@gmail.com>
2026-05-04 15:09:58 +0900

    入力フォームを追加

--- src/main/java/kakeibo/KakeiboController.java
+++ src/main/java/kakeibo/KakeiboController.java
@@ -3,20 +3,30 @@ package kakeibo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class KakeiboController {
 
     @GetMapping("/")
     public String index(Model model) {
-        int sunyuu = 243000;
-        int shishutsu = 41000;
-        int chokin = sunyuu - shishutsu;
-        
-        model.addAttribute("sunyuu", sunyuu);
-        model.addAttribute("shishutsu", shishutsu);
-        model.addAttribute("chokin", chokin);
-        
         return "index";
     }
+
+@PostMapping("/keisan")
+public String keisan(
+    @RequestParam int sunyuu,
+    @RequestParam int shishutsu,
+    Model model) {
+    
+    System.out.println("収入：" + sunyuu);
+    System.out.println("支出：" + shishutsu);
+    
+    int chokin = sunyuu - shishutsu;
+    System.out.println("貯金：" + chokin);
+    
+    model.addAttribute("chokin", chokin);
+    return "index";
+}
 }
